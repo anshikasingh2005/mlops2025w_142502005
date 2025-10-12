cba6e997fe4dc3851dd0abe82299657e5b860e58
Anshika Singh <142502005@smail.iitpkd.ac.in>
2025-10-12 22:25:53 +0530

    added pdf file paths

--- project/test.py
+++ project/test.py
@@ -13,7 +13,9 @@ load_dotenv()
 def test_document_ingestion_and_rag():
     try:
         test_files = [
-            "/Users/yashpatil/Developer/AI/YT/Sunny/LLMOps_series/data/NIPS-2017-attention-is-all-you-need-Paper.pdf",
+            "D:\projects\mlops2025w_142502005\project\data\NCERT-Class-6-History.pdf",
+            "D:\projects\mlops2025w_142502005\project\data\NCERT-Class-7-History.pdf",
+            "D:\projects\mlops2025w_142502005\project\data\NCERT-Class-8-History.pdf"
         ]
 
         uploaded_files = []
